85038fe116ab44d60862a1b43eb08e7580e919f3
Fatma <fceyda@gmail.com>
2020-09-03 12:06:09 -0700

    add UI logic
    Co-authored-by: Sara Kane <kane.sarak@gmail.com>
    Co-authored-by: Mariel Hamson <mariel.hamson@gmail.com>

--- Plant-RPG/src/main.js
+++ Plant-RPG/src/main.js
@@ -2,20 +2,34 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 import $ from 'jquery';
-import { stateControl, battleWithSquirrel } from './plant-rpg';
+import * as game from './plant-rpg';
 
 $(document).ready(function() {
+  const startingState = game.stateControl();
+  $('#water-value').text(`Water: ${startingState.water}`);
+  $('#seeds-value').text(`Seeds: ${startingState.seeds}`);
+  $('#petals-value').text(`Petals: ${startingState.petals}`);
+
+  game.plantWilts(game.stateControl);
+  game.waterLevel(game.stateControl);
+  setInterval(function() {
+    const newState = game.stateControl();
+    $('#seeds-value').text(`Seeds: ${newState.seeds}`);
+    $('#water-value').text(`Water: ${newState.water}`);
+  },1000);
+    
     $('#battle').click(function() {
-      const newState = stateControl(battleWithSquirrel(stateControl()['water']));
-      //battleWithSquirrel(plant()['water'])
-      console.log(newState);
+      const newState = game.stateControl(game.battleWithSquirrel(game.stateControl()['water']));
       $('#petals-value').text(`Petals: ${newState.petals}`);
     });
-  
-    $('#show-state').click(function() {
-      const currentState = stateControl();
-      $('#water-value').text(`Water: ${currentState.water}`);
-      $('#seeds-value').text(`Seeds: ${currentState.seeds}`);
-      $('#petals-value').text(`Petals: ${currentState.petals}`);
+    
+    $('#water').click(function() {
+      const newState = game.stateControl(game.increaseWater);
+      $('#water-value').text(`Water: ${newState.water}`);
+    });
+
+    $('#seeds').click(function() {
+      const newState = game.stateControl(game.increaseSeeds);
+      $('#seeds-value').text(`Seeds: ${newState.seeds}`);
     });
   });
