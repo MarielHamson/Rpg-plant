87154be13b3e61c6e95ff026801ee8a7f3c4ac24
Fatma <fceyda@gmail.com>
2020-09-03 09:49:06 -0700

    pass test for battle squarrel win
    Co-authored-by: MarielHamson <mariel.hamson@gmail.com>
    Co-authored-by: Sara Kane <kane.sarak@gmail.com>

--- Plant-RPG/__tests__/test.js
+++ Plant-RPG/__tests__/test.js
@@ -1,57 +1,79 @@
 import * as character from '../src/plant-rpg.js';
 
 describe('Character', () => {
-	jest.useFakeTimers();
-	test('should create new sunflower character', () => {
-		const initialState = { water: 10, seeds: 10, petals: 1 };
-		const plantName = character.storeState(initialState);
-		expect(plantName()).toMatchObject({ water: 10, seeds: 10, petals: 1 });
-	});
+  jest.useFakeTimers();
+  test('should create new sunflower character', () => {
+    const initialState = { water: 10, seeds: 10, petals: 1 };
+    const plantName = character.storeState(initialState);
+    expect(plantName()).toMatchObject({ water: 10, seeds: 10, petals: 1 });
+  });
 
-	test('should add a name', () => {
-		const initialState = { water: 10, seeds: 10, petals: 1 };
-		const plant = character.storeState(initialState);
-		const plantName = character.assignName('Sunflower');
-		const plantWithName = plant(plantName);
-		expect(plantWithName).toMatchObject({
-			water: 10,
-			seeds: 10,
-			petals: 1,
-			name: 'Sunflower',
-		});
-	});
+  test('should add a name', () => {
+    const initialState = { water: 10, seeds: 10, petals: 1 };
+    const plant = character.storeState(initialState);
+    const plantName = character.assignName('Sunflower');
+    const plantWithName = plant(plantName);
+    expect(plantWithName).toMatchObject({
+      water: 10,
+      seeds: 10,
+      petals: 1,
+      name: 'Sunflower',
+    });
+  });
 
-	test('battle decrements petals by 2', () => {
-		const initialState = { water: 10, seeds: 10, petals: 1 };
-		const plant = character.storeState(initialState);
-		expect(plant(character.battleLost)).toMatchObject({
-			water: 10,
-			seeds: 10,
-			petals: -1,
-		});
-	});
+  test('battle decrements petals by 2', () => {
+    const initialState = { water: 10, seeds: 10, petals: 1 };
+    const plant = character.storeState(initialState);
+    expect(plant(character.battleLost)).toMatchObject({
+      water: 10,
+      seeds: 10,
+      petals: -1,
+    });
+  });
 
-	test('battle squirrel', () => {
-		const initialState = { water: 10, seeds: 10, petals: 1 };
-		const plant = character.storeState(initialState);
-		expect(plant(character.battleWithSquirrel(plant()['water']))).toMatchObject(
-			{
-				water: 10,
-				seeds: 10,
-				petals: 2,
-			}
-		);
-	});
+  test('battle increases petals by 1', () => {
+    const initialState = { water: 10, seeds: 10, petals: 1 };
+    const plant = character.storeState(initialState);
+    expect(plant(character.battleWon)).toMatchObject({
+      water: 10,
+      seeds: 10,
+      petals: 2,
+    });
+  });
 
-	test('seeds decrement by 2 every 1000ms', () => {
-		const initialState = { water: 10, seeds: 10, petals: 1 };
-		const plant = character.storeState(initialState);
-		character.plantWilts(plant);
-		jest.advanceTimersByTime(1001);
-		expect(plant()).toMatchObject({
-			water: 10,
-			seeds: 8,
-			petals: 1,
-		});
-	});
+  test('battle squirrel win', () => {
+    const initialState = { water: 10, seeds: 10, petals: 1 };
+    const plant = character.storeState(initialState);
+    expect(plant(character.battleWithSquirrel(plant()['water']))).toMatchObject(
+      {
+        water: 10,
+        seeds: 10,
+        petals: 2,
+      }
+    );
+  });
+
+  test('battle squirrel loss', () => {
+    const initialState = { water: 9, seeds: 10, petals: 1 };
+    const plant = character.storeState(initialState);
+    expect(plant(character.battleWithSquirrel(plant()['water']))).toMatchObject(
+      {
+        water: 9,
+        seeds: 10,
+        petals: -1,
+      }
+    );
+  });
+
+  test('seeds decrement by 2 every 1000ms', () => {
+    const initialState = { water: 10, seeds: 10, petals: 1 };
+    const plant = character.storeState(initialState);
+    character.plantWilts(plant);
+    jest.advanceTimersByTime(1001);
+    expect(plant()).toMatchObject({
+      water: 10,
+      seeds: 8,
+      petals: 1,
+    });
+  });
 });
--- Plant-RPG/src/plant-rpg.js
+++ Plant-RPG/src/plant-rpg.js
@@ -1,29 +1,29 @@
 // import * as tools from './tools';
 export const storeState = (initialState) => {
-	let currentState = initialState;
-	return (stateChangeFunction = (state) => state) => {
-		const newState = stateChangeFunction(currentState);
-		currentState = { ...newState };
-		return newState;
-	};
+  let currentState = initialState;
+  return (stateChangeFunction = (state) => state) => {
+    const newState = stateChangeFunction(currentState);
+    currentState = { ...newState };
+    return newState;
+  };
 };
 
 export const changeState = (prop) => {
-	return (value) => {
-		return (state) => ({
-			...state,
-			[prop]: (state[prop] || 0) + value,
-		});
-	};
+  return (value) => {
+    return (state) => ({
+      ...state,
+      [prop]: (state[prop] || 0) + value,
+    });
+  };
 };
 //
 export const changeStringState = (prop) => {
-	return (value) => {
-		return (state) => ({
-			...state,
-			[prop]: value,
-		});
-	};
+  return (value) => {
+    return (state) => ({
+      ...state,
+      [prop]: value,
+    });
+  };
 };
 
 // const initialState = { water: 10, seeds: 10, petals: 1 };
@@ -32,24 +32,25 @@ export const assignName = changeStringState('name');
 // const plantName = assignName('Sunflower'); // changeStringState('name')('Sunflower')
 // const plantWithName = stateControl(plantName); //storeState(changeStringState('name')('Sunflower'))
 
-const battleWon = changeState('petals')(1);
+export const battleWon = changeState('petals')(1);
 export const battleLost = changeState('petals')(-2);
 
 export const battleWithSquirrel = (water) => {
-	if (water >= 10) {
-		return battleWon;
-	} else {
-		return battleLost;
-	}
+  if (water >= 10) {
+    return battleWon;
+  } else {
+    return battleLost;
+  }
 };
 
 const changeSeeds = changeState('seeds')(-2);
 
 export const plantWilts = (state) => {
-	setInterval(() => {
-		state(changeSeeds);
-	}, 1000);
+  setInterval(() => {
+    state(changeSeeds);
+  }, 1000);
 };
+
 
 // setHunger() {
 //   let hungerInterval = setInterval(() => {
